cff315b980eebaaeb21e045fcec2f062db2ec2da
Eduardo377 <eduardogomes377@gmail.com>
2025-03-21 18:38:29 -0300

    :recycle: refactor: rafatoracao do código no login

--- src/components/solicitacoes/table/table.jsx
+++ src/components/solicitacoes/table/table.jsx
@@ -1,7 +1,7 @@
 import style from './table.module.scss';
 
-import bin from '../../../assets/Dashboard/bin.png';
-import descriptionReason from '../../../assets/Dashboard/descriptionReason.png';
+import bin from '../../../assets/Dashboard/binVentor.png';
+import descriptionReason from '../../../assets/Dashboard/descriptionReasonVector.png';
 
 export default function Table() {
     return (
@@ -28,22 +28,67 @@ export default function Table() {
                 <td>
                     <img src={bin} alt="Lixeira para exclusão" />
                 </td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>Vitor Carvalho de Souza</td>
+                <td>WSS001</td>
+                <td>329456</td>
+                <td>08/01/2025</td>
+                <td>
+                    <img src={descriptionReason} alt="Descrição do motivo" />
+                </td>
+                <td>Desp. de viagem administrativa</td>
+                <td>1100110002 - FIN VICE-PRESIDENCIA FINANCAS MTZ</td>
+                <td>0003</td>
+                <td>002</td>
+                <td>001</td>
+                <td>BRL</td>
+                <td>434Km</td>
+                <td>0.65</td>
+                <td>242.10</td>
+                <td>40.05</td>
+            </tr>
+            <tr>
+                <td>
+                    <img src={bin} alt="Lixeira para exclusão" />
+                </td>
+                <td>Vanessa Portugal</td>
+                <td>WSS002</td>
+                <td>987789</td>
+                <td>01/01/2025</td>
+                <td>
+                    <img src={descriptionReason} alt="Descrição do motivo" />
+                </td>
+                <td>Desp. de viagem administrativa</td>
+                <td>1100110102 - FIN CONTABILIDADE MTZ</td>
+                <td>0002</td>
+                <td>005</td>
+                <td>001</td>
+                <td>ARS</td>
+                <td>289Km</td>
+                <td>0.37</td>
+                <td>106.93</td>
+                <td>0.00</td>
+            </tr>
+            <tr>
+                <td>
+                    <img src={bin} alt="Lixeira para exclusão" />
+                </td>
+                <td>Washington Klinglon</td>
+                <td>WSS003</td>
+                <td>546791</td>
+                <td>03/01/2025</td>
+                <td>
+                    <img src={descriptionReason} alt="Descrição do motivo" />
+                </td>
+                <td>Eventos de apresentação</td>
+                <td>1100109002 - FIN CONTROLES INTERNOS MTZ</td>
+                <td>0001</td>
+                <td>005</td>
+                <td>001</td>
+                <td>USD</td>
+                <td>197Km</td>
+                <td>0.75</td>
+                <td>109.75</td>
+                <td>29.97</td>
             </tr>
         </table>
     )
