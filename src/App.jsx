7d29326e6329b6c278e9babbd19434528d905a50
Eduardo377 <eduardogomes377@gmail.com>
2025-04-08 17:58:46 -0300

    :bug: fix: rota 404 corrigida com rewrite para /

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login.jsx";
 import Home from "./pages/home/Home.jsx";
