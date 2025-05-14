64bd368370f9cc1457fc75dcf17dfe476fad008a
Eduardo377 <eduardogomes377@gmail.com>
2025-05-14 06:02:08 -0300

    Remover src/Services/Api.jsx

--- src/Services/Api.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-//Cria uma instância do axios, chamada "api"
-const api = axios.create({
-    baseURL: "http://localhost:5000" //Define a URL base para todas as requisições feitas com essa instância.
-})
-
-export default api;
